346c7c999ba32a76ef8903132b5dd2db78187fba
Georgiy Gaidarenko <gaydar@atim.ru>
2025-08-17 15:21:55 +0300

    Fix after review

--- src/shared/ui/checkbox/checkbox.tsx
+++ src/shared/ui/checkbox/checkbox.tsx
@@ -1,20 +1,18 @@
 import styles from './checkbox.module.scss';
-import type { ReactNode } from 'react';
-
-type CheckboxProps = {
-  checked: boolean;
-  onChange?: () => void;
-  variant?: 'minus';
-  children?: ReactNode;
-}
+import type { CheckboxProps } from './types';
+import IconCheckboxEmpty from '@icons/ui/checkbox-empty.svg?react';
+import IconCheckboxDone from '@icons/ui/checkbox-done.svg?react';
+import IconCheckboxRemove from '@icons/ui/checkbox-remove.svg?react';
 
 export const Checkbox = (props: CheckboxProps) => {
   const { checked, variant, onChange, children } = props;
 
+  const IconChecked = variant === 'minus' ? IconCheckboxRemove : IconCheckboxDone;
+
   return (
 	<label className={styles.container}>
 	  <input type='checkbox' className={styles.hidden} checked={checked} onChange={onChange} />
-	  <span className={styles.checkmark} data-variant={variant} ></span>
+	  { checked ? <IconChecked className={styles.checked} /> : <IconCheckboxEmpty className={styles.empty} /> }
 	  {children}
 	</label>
   );
