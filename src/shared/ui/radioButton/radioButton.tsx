5a3ee498a1349d0ff431bc1887964c3fb5a410ca
Daiana <edinoroshechka@yandex.com>
2025-08-17 17:51:20 +0300

    fix: rewrite radio add svg Icon

--- src/shared/ui/radioButton/radioButton.tsx
+++ src/shared/ui/radioButton/radioButton.tsx
@@ -2,6 +2,9 @@ import { type FC } from 'react';
 import * as React from 'react';
 import type { RadioUiProps } from './type';
 import styles from './radioButton.module.scss';
+import IconActive from '../../assets/icons/ui/radiobutton-active.svg?react';
+import IconEmpty from '../../assets/icons/ui/radiobutton-empty.svg?react';
+
 // eslint-disable-next-line import-x/prefer-default-export
 export const RadioButton: FC<RadioUiProps> = ({
   id,
@@ -17,7 +20,7 @@ export const RadioButton: FC<RadioUiProps> = ({
   };
 
   return (
-    <div className={styles.radio}>
+    <label htmlFor={id} className={styles.radio}>
       <input
         type='radio'
         id={id}
@@ -28,7 +31,12 @@ export const RadioButton: FC<RadioUiProps> = ({
         disabled={disabled}
         className={styles.radio__input}
       />
-      <label htmlFor={id}> {label} </label>
-    </div>
+      {checked ? (
+        <IconActive className={styles.radio__input__icon} />
+      ) : (
+        <IconEmpty className={styles.radio__input__icon} />
+      )}
+      <span className={styles.radio__input__label}>{label}</span>
+    </label>
   );
 };
