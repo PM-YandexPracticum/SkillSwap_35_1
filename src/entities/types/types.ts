1bb8a52924b65c2234286aa3c1bf15a3cd4dfa6a
Danila Niurkin <dnurkin@gmail.com>
2025-08-18 12:55:40 +0300

    feat: добавлены типы навыка и пользователя

---
+++ src/entities/types/types.ts
@@ -0,0 +1,25 @@
+export interface ISkill {
+    category: string;
+    subcategory: string;
+    title: string;
+    description: string;
+    images?: string[];
+};
+
+export interface IUser {
+    id: string;
+    email: string;
+    password: string;
+    name: string;
+    about: string;
+    gender: 'male' | 'female';
+    dateOfBirth: string;
+    city: string;
+    image?: string;
+    can: ISkill;
+    want: string[];
+    likeCount: number;
+    createdAt: string;
+};
+
+export interface IUserPublic extends Omit<IUser, 'email' | 'password'> {};
