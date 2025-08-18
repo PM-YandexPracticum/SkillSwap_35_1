d76f69889e352e570081267513e246c693d80bf4
Danila Niurkin <dnurkin@gmail.com>
2025-08-18 14:49:21 +0300

    build: установлен React Router

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import './index.scss';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import store from './app/services/store';
 import App from './app/App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
