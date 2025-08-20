0132af57511014e957e23f9d411f590bc5d6f4e1
Danila Niurkin <dnurkin@gmail.com>
2025-08-20 12:32:22 +0300

    refactor: типизирована PayloadAction в слайсе skillsSlice

--- src/app/services/slices/skillsSlice.ts
+++ src/app/services/slices/skillsSlice.ts
@@ -1,4 +1,5 @@
 import { createSelector, createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
 import type { IUserPublic } from "src/entities/types/types";
 
 export type TSkillsState = {
@@ -13,7 +14,7 @@ export const SkillSlice = createSlice({
   name: 'skills',
   initialState,
   reducers: {
-    setSkills: (state, action) => {
+    setSkills: (state, action: PayloadAction<IUserPublic[]>) => {
       state.skills = action.payload;
     }
   },
