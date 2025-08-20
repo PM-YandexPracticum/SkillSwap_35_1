b58f6cfa1a99ca9c5d2fbfbf0725d11e45a71fa5
Alexandr Sokolov <alexandrsokolov.dev@gmail.com>
2025-08-20 17:11:20 +0300

    feat/skillsMockThunk: добавлен моковый thunk для подгрузки скиллов из users.json

--- src/app/services/slices/skillsSlice.ts
+++ src/app/services/slices/skillsSlice.ts
@@ -1,14 +1,58 @@
-import { createSelector, createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from "@reduxjs/toolkit";
-import type { IUserPublic } from "src/entities/types/types";
+/* eslint-disable no-param-reassign */
+import {
+  createSelector,
+  createSlice,
+  createAsyncThunk
+} from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import type { IUserPublic } from 'src/entities/types/types';
+import mockSkills from '../../../../public/db/users.json';
+import { multiplyArrayElements } from '../../../utils';
 
 export type TSkillsState = {
   skills: IUserPublic[];
+  loading: boolean;
+  error: string | null;
+  hasMore: boolean;
 };
 
 const initialState: TSkillsState = {
   skills: [],
+  loading: false,
+  error: null,
+  hasMore: true
 };
+
+export const getMockSkills = createAsyncThunk(
+  'skills/getMockCards',
+  async (startIndex: number, { rejectWithValue }) => {
+    try {
+      await new Promise((resolve) => {
+        setTimeout(resolve, 1000);
+      });
+
+      const skillsData: IUserPublic[] = multiplyArrayElements(mockSkills);
+
+      const pageSize = 20;
+      const pageStart = startIndex;
+      const pageEnd = startIndex + pageSize;
+
+      if (pageStart >= skillsData.length) {
+        return {
+          skills: [],
+          hasMore: false
+        };
+      }
+
+      return {
+        skills: skillsData.slice(pageStart, pageEnd),
+        hasMore: true
+      };
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
 
 export const SkillSlice = createSlice({
   name: 'skills',
@@ -20,6 +64,24 @@ export const SkillSlice = createSlice({
   },
   selectors: {
     getSkills: (state) => state.skills
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(getMockSkills.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getMockSkills.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+
+        state.skills = [...state.skills, ...action.payload.skills];
+        state.hasMore = action.payload.hasMore;
+      })
+      .addCase(getMockSkills.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string;
+      });
   }
 });
 
