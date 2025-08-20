c217eeb46820fad9eb332ee6bafdd6796f308f96
Danila Niurkin <dnurkin@gmail.com>
2025-08-20 10:50:27 +0300

    feat: добавлен компонент Preloader

--- src/app/components/infinite-grid/InfiniteGrid.tsx
+++ src/app/components/infinite-grid/InfiniteGrid.tsx
@@ -3,6 +3,7 @@ import type { InfiniteGridProps } from './types';
 import type { IUserPublic } from '../../../entities/types/types';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { Title } from '@ui/title/Title';
+import { Preloader } from '@ui/preloader';
 import { SkillCard } from '../../../widgets/skill-card';
 import styles from './Infinite.module.scss';
 
@@ -18,29 +19,33 @@ export const InfiniteGrid: React.FC<InfiniteGridProps> = ({ title }) => {
       const newId = (startIndex + i).toString();
 
       return {
-      id: newId,
-      name: 'Маэстро',
-      about: 'Единственный и неповторимый',
-      gender: 'male',
-      dateOfBirth: '1988-07-14',
-      city: 'Санкт-Петербург',
-      image: 'https://distribution.faceit-cdn.net/images/5c0cd4f9-1b09-4c0a-9061-7c2c0edd24cb.jpeg',
-      can: {
-        title: 'Маркетинг SEO и SMM',
-        category: 'Бизнес и карьера',
-        subcategory: 'Маркетинг и реклама',
-        description: 'Научу заниматься SEO и SMM. Выстраивать бренд и таргетировать рекламу',
-        images: ['https://distribution.faceit-cdn.net/images/5c0cd4f9-1b09-4c0a-9061-7c2c0edd24cb.jpeg']
-      },
-      want: [
-        { subcategory: 'Тайм-менеджмент', category: 'Бизнес и карьера' },
-        { subcategory: 'Медитация', category: 'Здоровье и лайфстайл' },
-        { subcategory: 'Скорочтение', category: 'Образование и развитие' },
-        { subcategory: 'Фотография', category: 'Творчество и искусство' }
-      ],
-      likeCount: 1000000,
-      createdAt: 'сегодня'
-    };
+        id: newId,
+        name: 'Маэстро',
+        about: 'Единственный и неповторимый',
+        gender: 'male',
+        dateOfBirth: '1988-07-14',
+        city: 'Санкт-Петербург',
+        image:
+          'https://distribution.faceit-cdn.net/images/5c0cd4f9-1b09-4c0a-9061-7c2c0edd24cb.jpeg',
+        can: {
+          title: 'Маркетинг SEO и SMM',
+          category: 'Бизнес и карьера',
+          subcategory: 'Маркетинг и реклама',
+          description:
+            'Научу заниматься SEO и SMM. Выстраивать бренд и таргетировать рекламу',
+          images: [
+            'https://distribution.faceit-cdn.net/images/5c0cd4f9-1b09-4c0a-9061-7c2c0edd24cb.jpeg'
+          ]
+        },
+        want: [
+          { subcategory: 'Медитация', category: 'Здоровье и лайфстайл' },
+          { subcategory: 'Скорочтение', category: 'Образование и развитие' },
+          { subcategory: 'Тайм-менеджмент', category: 'Бизнес и карьера' },
+          { subcategory: 'Фотография', category: 'Творчество и искусство' }
+        ],
+        likeCount: 1000000,
+        createdAt: 'сегодня'
+      };
     });
   };
   // Здесь будет запрос ( в нашем случае подгрузка) данных + стоит setTimeout для проверки работы
@@ -68,7 +73,7 @@ export const InfiniteGrid: React.FC<InfiniteGridProps> = ({ title }) => {
         dataLength={cards.length}
         next={fetchData}
         hasMore={hasMore}
-        loader={<p>Загружаем ещё пользователей...</p>}
+        loader={<Preloader />}
         scrollThreshold={0.8}
         style={{ overflow: 'hidden' }}
       >
