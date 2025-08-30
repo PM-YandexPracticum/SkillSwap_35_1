0b55b7fc0dcde3ed5377314d11f331d775b2ba8f
Georgiy Gaidarenko <gaydar@atim.ru>
2025-08-30 12:06:18 +0300

    Fix after rebase

--- src/pages/skill-page/SkillPage.tsx
+++ src/pages/skill-page/SkillPage.tsx
@@ -9,20 +9,13 @@ import { getSkillById, getSkills, getSimilarSkills } from '@entities/skill/model
 import type { IUser, IUserPublic } from '@entities/user/model/types/types';
 import { getUserData, toggleFavorites } from '@entities/user/model/user-slice/userSliсe';
 
-// #TODO: Убрать когда разберёмся с неправильными ссылками на картинки. пути к картинкам должны начинаться с '/'
-const fixImgPath = (path: string) => (!path || path[0] === '/' ? path : '/' + path);
-
 export const SkillPage = () => {
   useEffect(() => { window.scrollTo(0, 0) }, []);
   const { id } = useParams();
   const dispatch = useDispatch();
 
   const skill: IUserPublic | undefined = useSelector(getSkillById(id || ''));
-  let similarSkills = useSelector(getSkills); // #TODO: Заменить на getSimilarSkills
-
-  // #TODO: Убрать когда разберёмся с неправильными ссылками на картинки
-  similarSkills = similarSkills.map(item => ({...item, image: item.image && fixImgPath(item.image) }));
-
+  const similarSkills = useSelector(getSkills); // #TODO: Заменить на getSimilarSkills
   const user: IUser | null = useSelector(getUserData);
 
   if (!skill) {
@@ -39,7 +32,7 @@ export const SkillPage = () => {
           userDateofBirth={skill.dateOfBirth}
           userSkillCategory={skill.can.category}
           userSkillName={skill.can.title}
-          userPhotoUrl={skill.image && fixImgPath(skill.image)}
+          userPhotoUrl={skill.image}
           skillsToLearn={skill.want}
           userAbout={skill.about}
         />
@@ -47,7 +40,7 @@ export const SkillPage = () => {
           title={skill.can.title}
           subTitle={`${skill.can.category} / ${skill.can.subcategory}`}
           text={skill.can.description}
-          images={skill.can.images ? skill.can.images.map(img => fixImgPath(img)) : []}
+          images={skill.can.images || []}
           variant='want'
           isLikeActive={!!user}
           isLiked={!!user && user.favorites.includes(skill.id)}
