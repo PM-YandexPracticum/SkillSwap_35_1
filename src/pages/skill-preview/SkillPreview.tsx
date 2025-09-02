b8d886b6073977237be69f5962ba70006a155cdd
Danila Niurkin <dnurkin@gmail.com>
2025-09-03 02:10:54 +0300

    fix: убраны пропсы у SkillPreview

--- src/pages/skill-preview/SkillPreview.tsx
+++ src/pages/skill-preview/SkillPreview.tsx
@@ -2,32 +2,38 @@ import styles from './SkillPreview.module.scss'
 import { Title } from '@ui/title/Title';
 import { Text } from '@ui/text/Text';
 import { SkillDetails } from '@entities/skill/ui/skill-details';
-import type { SkillPreviewProps } from './types';
+import { useSelector } from '../../app/providers/store/store';
+import { getUserData } from '@entities/user/model/user-slice/userSliсe';
 
-export const SkillPreview = (props: SkillPreviewProps) => {
-  const {
-    title,
-    subTitle,
-    text,
-    images,
-    onEditClick,
-    onDoneClick
-  } = props;
+export const SkillPreview = () => {
+  
+  const user = useSelector(getUserData);
+  const skill = user?.can;
+
+  if (!user || !skill ) {
+    return (
+     <div className={styles.container_empty}>
+            <Text tag='span' size='main'>
+              Навык не найден
+            </Text>
+          </div>
+  )
+}
 
   return (
     <div className={styles.container}>
       <div className={styles.title}>
         <Title tag='h3'>Ваше предложение</Title>
-        <Text tag='p' size='details'>Пожалуйста, проверьте и подтвердите правильность данных</Text>
+        <Text tag='p' size='details'>Пожалуйста, проверьте и подтвердите правильность данных</Text>
       </div>
       <SkillDetails
-        title={title}
-        subTitle={subTitle}
-        text={text}
-        images={images}
+        title={skill.title}
+        subTitle={`${skill.category} / ${skill.subcategory}`}
+        text={skill.description}
+        images={skill.images || []}
         variant='can'
-        onEditClick={onEditClick}
-        onDoneClick={onDoneClick}
+        onEditClick={() => null}
+        onDoneClick={() => null}
       />
     </div>
   );
--- src/pages/skill-preview/types.ts
+++
@@ -1,8 +0,0 @@
-export type SkillPreviewProps = {
-  title: string;
-  subTitle: string;
-  text: string;
-  images: string[];
-  onEditClick?: () => void;
-  onDoneClick?: () => void;
-}
